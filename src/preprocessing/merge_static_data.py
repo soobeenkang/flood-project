dc02cdab275304fb14b743e7e19d6d5b47eee136
강수빈 <ksbkitty2@naver.com>
2026-04-02 15:06:19 +0900

    UPDATE merge_static - FIX: grid filtering 주석처리

--- src/preprocessing/merge_static_data.py
+++ src/preprocessing/merge_static_data.py
@@ -1,12 +1,19 @@
 """
     static data인 elevation, is_river를
-    침수 이력이 있는 grid_id 기준으로 머지해
+    선택: "침수 이력이 있는 grid_id 기준으로" 
+    머지해
     하나의 파일로 저장하는 코드
     최종: grid_id, elevation, is_river
 """
 import pandas as pd
 
 def merge_static_data():
+
+    elev = pd.read_parquet("data/final/seoul_grid_with_elevation.parquet")
+    river = pd.read_parquet("data/final/seoul_grid_with_river_flag.parquet")
+    river["is_river"] = river["is_river"].astype("int8")
+
+    """
     # flood 읽어와서 침수 이력 있는 그리드만 남겨두기
     flood = pd.read_parquet("data/final/seoul_final_flood_grid.parquet")
     flood["F_SAT_YMD"] = pd.to_datetime(flood["F_SAT_YMD"])
@@ -18,11 +25,8 @@ def merge_static_data():
 
     flood_grids = flood["grid_id"].unique()
 
-    elev = pd.read_parquet("data/final/seoul_grid_with_elevation.parquet")
-    river = pd.read_parquet("data/final/seoul_grid_with_river_flag.parquet")
-    river["is_river"] = river["is_river"].astype("int8")
-
     elev = elev[elev["grid_id"].isin(flood_grids)]
+    """
 
     elev = elev.merge(river, on=["grid_id"], how="left")
 
